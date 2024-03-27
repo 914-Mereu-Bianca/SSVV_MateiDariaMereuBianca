9158eaf076b2365637d35871bb670aedbb308122
914-Mereu-Bianca <biamereu2002@gmail.com>
2024-03-27 21:11:58 +0200

    .

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,76 @@
+package org.example;
+
+
+import junit.framework.TestCase;
+import org.example.domain.Student;
+import org.example.repository.NotaXMLRepo;
+import org.example.repository.StudentXMLRepo;
+import org.example.repository.TemaXMLRepo;
+import org.example.service.Service;
+import org.example.validation.NotaValidator;
+import org.example.validation.StudentValidator;
+import org.example.validation.TemaValidator;
+import org.example.validation.ValidationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+    extends TestCase
+{
+
+    public static Service service;
+
+    @BeforeAll
+    public static void setup() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "src/fisiere/Studenti.xml";
+        String filenameTema = "src/fisiere/Teme.xml";
+        String filenameNota = "src/fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @Test
+    public void addStudent_ValidOperation() {
+        String idStudent = "test";
+        String numeStudent = "Ana";
+        int grupa = 934;
+        String email = "anaaa@yahoo.com";
+        Student student = new Student(idStudent, numeStudent, grupa, email);
+
+        try {
+            service.addStudent(student);
+        } catch (ValidationException exception) {
+            System.out.println(exception);
+            assertFalse(true);
+        }
+
+        assert(service.findStudent(idStudent) != null);
+    }
+
+    @Test
+    public void addStudent_ErrorGroup_ThrowError() {
+        String idStudent = "90";
+        String numeStudent = "Dan";
+        int grupa = -9;
+        String email = "dani@yahoo.com";
+        Student student = new Student(idStudent, numeStudent, grupa, email);
+
+        try {
+            service.addStudent(student);
+            assert(false);
+        } catch (ValidationException exception) {
+            System.out.println(exception);
+            assert(true);
+        }
+    }
+
+}
